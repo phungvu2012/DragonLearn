bf24db7637302f6bf4b40821545397a979f60ffa
nmsang18hm <57668387+nmsang18hm@users.noreply.github.com>
2020-05-11 16:55:34 +0700

    update #10
    Hoàn thiện các màn chơi liên tiếp

--- views/JS/10_berries.js
+++ views/JS/10_berries.js
@@ -18,18 +18,62 @@ function outBox(objtail, objhead) {
     	}, i*5 );
 	}
 }
+
+function complete(arrayT, arrayH) {
+	var kq = true;
+	for (var i = 0; i < arrayT.length; i++) {
+		if(arrayT[i] != -1) {
+			kq = false;
+			break;
+		}
+	}
+	return kq;
+}
+
+function loadStage(stage, currNOT, currNOH, arrT, arrH, objtail, objhead) {
+	currNOT[0] = arrT[3*stage - 6];
+	currNOT[1] = arrT[3*stage - 5];
+	currNOT[2] = arrT[3*stage - 4];
+	currNOH[0] = arrH[3*stage - 6];
+	currNOH[1] = arrH[3*stage - 5];
+	currNOH[2] = arrH[3*stage - 4];
+	objtail[0].style.backgroundImage = "url('/server/Image/Load%20trucks%20with%2010%20berries/duoi_xe" + currNOT[0] + ".png')";
+	objtail[0].style.left = "100px";
+	objtail[0].style.top = "17px";
+	objtail[0].style.width = "170px";
+	objtail[1].style.backgroundImage = "url('/server/Image/Load%20trucks%20with%2010%20berries/duoi_xe" + currNOT[1] + ".png')";
+	objtail[1].style.left = "100px";
+	objtail[1].style.top = "197px";
+	objtail[1].style.width = "170px";
+	objtail[2].style.backgroundImage = "url('/server/Image/Load%20trucks%20with%2010%20berries/duoi_xe" + currNOT[2] + ".png')";
+	objtail[2].style.left = "100px";
+	objtail[2].style.top = "374px";
+	objtail[2].style.width = "170px";
+	objhead[0].style.backgroundImage = "url('/server/Image/Load%20trucks%20with%2010%20berries/dau_xe" + currNOH[0] + ".png')";
+	objhead[0].style.left = "675px";
+	objhead[0].style.top = "65px";
+	objhead[0].style.width = "215px";
+	objhead[1].style.backgroundImage = "url('/server/Image/Load%20trucks%20with%2010%20berries/dau_xe" + currNOH[1] + ".png')";
+	objhead[1].style.left = "675px";
+	objhead[1].style.top = "245px";
+	objhead[1].style.width = "215px";
+	objhead[2].style.backgroundImage = "url('/server/Image/Load%20trucks%20with%2010%20berries/dau_xe" + currNOH[2] + ".png')";
+	objhead[2].style.left = "675px";
+	objhead[2].style.top = "422px";
+	objhead[2].style.width = "215px";
+}
 // Main code
 $(document).ready(function() {
+	//Các object trong bài (Chưa tối ưu)
 	var objKhung = document.getElementById("khung");
-	var objtail1 = document.getElementById("tail1");
-	var objtail2 = document.getElementById("tail2");
-	var objtail3 = document.getElementById("tail3");
-	var objhead1 = document.getElementById("head1");
-	var objhead2 = document.getElementById("head2");
-	var objhead3 = document.getElementById("head3");
-
+	var objtail = document.getElementsByClassName("tail");
+	var objhead = document.getElementsByClassName("head");
+	// Các biến thành phần của màn chơi
+	var stage = 1;
 	var currentNumberOnTail = [5, 8, 4];
 	var currentNumberOnHead = [2, 6, 5];
+	var arrayTail = [1, 6, 7, 5, 9, 3, 7, 5, 6];
+	var arrayHead = [3, 9, 4, 7, 1, 5, 4, 3, 5];
 
 
 	$('.tail').draggable({
@@ -69,8 +113,8 @@ $(document).ready(function() {
         //$('#posX').text('x: ' + xPos);
         //$('#posY').text('y: ' + yPos);
 		if(yPos >= yPosKhung && yPos <= yPosKhung + 82 && xPos >= xPosKhung + 490) {
-			objtail1.style.left = "505px";
-			objtail1.style.top = "17px";
+			objtail[0].style.left = "505px";
+			objtail[0].style.top = "17px";
 			if(currentNumberOnTail[0] + currentNumberOnHead[0] != 10) {
 				$('#tail1').delay(500).animate(
 					{
@@ -79,12 +123,22 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail1, objhead1);
+			else {
+				currentNumberOnTail[0] = -1;
+				currentNumberOnHead[0] = -1;
+				outBox(objtail[0], objhead[0]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
 		else
 		if(yPos >= yPosKhung + 132 && yPos <= yPosKhung + 262 && xPos >= xPosKhung + 490) {
-			objtail1.style.left = "505px";
-			objtail1.style.top = "197px";
+			objtail[0].style.left = "505px";
+			objtail[0].style.top = "197px";
 			if(currentNumberOnTail[0] + currentNumberOnHead[1] != 10) {
 				$('#tail1').delay(500).animate(
 					{
@@ -93,12 +147,22 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail1, objhead2);
+			else {
+				currentNumberOnTail[0] = -1;
+				currentNumberOnHead[1] = -1;
+				outBox(objtail[0], objhead[1]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
 		else
 		if(yPos >= yPosKhung + 312 && yPos <= yPosKhung + 442 && xPos >= xPosKhung + 490) {
-			objtail1.style.left = "505px";
-			objtail1.style.top = "374px";
+			objtail[0].style.left = "505px";
+			objtail[0].style.top = "374px";
 			if(currentNumberOnTail[0] + currentNumberOnHead[2] != 10) {
 				$('#tail1').delay(500).animate(
 					{
@@ -107,7 +171,17 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail1, objhead3);
+			else {
+				currentNumberOnTail[0] = -1;
+				currentNumberOnHead[2] = -1;
+				outBox(objtail[0], objhead[2]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
     }
 	});
@@ -123,8 +197,8 @@ $(document).ready(function() {
         //$('#posX').text('x: ' + xPos);
         //$('#posY').text('y: ' + yPos);
 		if(yPos >= yPosKhung && yPos <= yPosKhung + 82 && xPos >= xPosKhung + 490) {
-			objtail2.style.left = "505px";
-			objtail2.style.top = "17px";
+			objtail[1].style.left = "505px";
+			objtail[1].style.top = "17px";
 			if(currentNumberOnTail[1] + currentNumberOnHead[0] != 10) {
 				$('#tail2').delay(500).animate(
 					{
@@ -133,12 +207,22 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail2, objhead1);
+			else {
+				currentNumberOnTail[1] = -1;
+				currentNumberOnHead[0] = -1;
+				outBox(objtail[1], objhead[0]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
 		else
 		if(yPos >= yPosKhung + 132 && yPos <= yPosKhung + 262 && xPos >= xPosKhung + 490) {
-			objtail2.style.left = "505px";
-			objtail2.style.top = "197px";
+			objtail[1].style.left = "505px";
+			objtail[1].style.top = "197px";
 			if(currentNumberOnTail[1] + currentNumberOnHead[1] != 10) {
 				$('#tail2').delay(500).animate(
 					{
@@ -147,12 +231,22 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail2, objhead2);
+			else {
+				currentNumberOnTail[1] = -1;
+				currentNumberOnHead[1] = -1;
+				outBox(objtail[1], objhead[1]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
 		else
 		if(yPos >= yPosKhung + 312 && yPos <= yPosKhung + 442 && xPos >= xPosKhung + 490) {
-			objtail2.style.left = "505px";
-			objtail2.style.top = "374px";
+			objtail[1].style.left = "505px";
+			objtail[1].style.top = "374px";
 			if(currentNumberOnTail[1] + currentNumberOnHead[2] != 10) {
 				$('#tail2').delay(500).animate(
 					{
@@ -161,7 +255,17 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail2, objhead3);
+			else {
+				currentNumberOnTail[1] = -1;
+				currentNumberOnHead[2] = -1;
+				outBox(objtail[1], objhead[2]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
     }
 	});
@@ -177,8 +281,8 @@ $(document).ready(function() {
         //$('#posX').text('x: ' + xPos);
         //$('#posY').text('y: ' + yPos);
 		if(yPos >= yPosKhung && yPos <= yPosKhung + 82 && xPos >= xPosKhung + 490) {
-			objtail3.style.left = "505px";
-			objtail3.style.top = "17px";
+			objtail[2].style.left = "505px";
+			objtail[2].style.top = "17px";
 			if(currentNumberOnTail[2] + currentNumberOnHead[0] != 10) {
 				$('#tail3').delay(500).animate(
 					{
@@ -187,12 +291,22 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail3, objhead1);
+			else {
+				currentNumberOnTail[2] = -1;
+				currentNumberOnHead[0] = -1;
+				outBox(objtail[2], objhead[0]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
 		else
 		if(yPos >= yPosKhung + 132 && yPos <= yPosKhung + 262 && xPos >= xPosKhung + 490) {
-			objtail3.style.left = "505px";
-			objtail3.style.top = "197px";
+			objtail[2].style.left = "505px";
+			objtail[2].style.top = "197px";
 			if(currentNumberOnTail[2] + currentNumberOnHead[1] != 10) {
 				$('#tail3').delay(500).animate(
 					{
@@ -201,12 +315,22 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail3, objhead2);
+			else {
+				currentNumberOnTail[2] = -1;
+				currentNumberOnHead[1] = -1;
+				outBox(objtail[2], objhead[1]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
 		else
 		if(yPos >= yPosKhung + 312 && yPos <= yPosKhung + 442 && xPos >= xPosKhung + 490) {
-			objtail3.style.left = "505px";
-			objtail3.style.top = "374px";
+			objtail[2].style.left = "505px";
+			objtail[2].style.top = "374px";
 			if(currentNumberOnTail[2] + currentNumberOnHead[2] != 10) {
 				$('#tail3').delay(500).animate(
 					{
@@ -215,7 +339,17 @@ $(document).ready(function() {
 					}, 800
 				);
 			}
-			else outBox(objtail3, objhead3);
+			else {
+				currentNumberOnTail[2] = -1;
+				currentNumberOnHead[2] = -1;
+				outBox(objtail[2], objhead[2]);
+				if(complete(currentNumberOnTail, currentNumberOnHead)) {
+					setTimeout(function() {
+						stage++;
+						loadStage(stage, currentNumberOnTail, currentNumberOnHead, arrayTail, arrayHead, objtail, objhead);
+					}, 3000);
+				}
+			}
 		}
     }
 	});
