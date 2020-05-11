9e5f3c2bbcc8cff5663e6fe5e4bd261ada91b72d
phungvu2012 <phungvuminh2012@gmail.com>
2020-05-11 09:04:15 +0700

    updata event function #4

--- views/JS/createElement_LoadTheTrucks.js
+++ views/JS/createElement_LoadTheTrucks.js
@@ -1,5 +1,18 @@
+/* Đã xong */
 //Khởi tạo các phần tử
-var data = { left: [2, 1, 4, 3], right: [6, 7, 8, 9, 10] };
+var numbersLesson = 
+	{ 
+		left: 
+		[
+			[2, 1, 4, 3]
+		],
+ 		right: 
+ 		[
+ 			[6, 7, 8, 9, 10] 
+ 		]
+ 	};
+
+// Tạo hàng trên xe
 function createBricks(numbers) {
 	var brickHolder = document.getElementsByClassName('brick_holder')[0];
 	for (let i = 0, length = numbers.length; i < length; i++) {
@@ -17,12 +30,14 @@ function createBricks(numbers) {
 		brickHolder.appendChild(newLoadedBrick);
 	}
 }
-
+// Tạo hàng xếp vào
 function createToLoadBricks(numbers) {
 	var cont = document.getElementsByClassName('cont')[0];
 	for (let i = 0, length = numbers.length; i < length; i++) {
 		var newToLoadedBrick = document.createElement('div');
 		newToLoadedBrick.setAttribute('class','to_load_brick under_start');
+		newToLoadedBrick.classList.add('element' + i);
+		// Giá trị đầu tiên là 20 vì cách lề thêm 4px để dễ nhìn hơn;
 		newToLoadedBrick.setAttribute('style','height:' +  (20 + 24 * (numbers[i] - 1)).toFixed(1) + 'px; left: ' + (147 + 65 * i).toFixed(1) + 'px; top: ' + (323 - (20 + 24 * (numbers[i] - 1))) + 'px; touch-action: none;');
 		newToLoadedBrick.setAttribute('draggable','true');
 		var newCap = document.createElement('div');
--- views/JS/loadthetrucks.js
+++ views/JS/loadthetrucks.js
@@ -22,8 +22,76 @@ function startLesson() {
 	}
 }
 
+
+
+/* Đã xong */
+//Khởi tạo các phần tử
+var data = 
+	{ 
+		left: 
+		[
+			[2, 1, 4, 3]
+		],
+ 		right: 
+ 		[
+ 			[6, 7, 8, 9, 10] 
+ 		]
+ 	};
+
+// Tạo hàng trên xe
+function createBricks(numbers) {
+	var brickHolder = document.getElementsByClassName('brick_holder')[0];
+	for (let i = 0, length = numbers.length; i < length; i++) {
+		var newLoadedBrick = document.createElement('div');
+		newLoadedBrick.setAttribute('class','loaded_brick');
+		newLoadedBrick.setAttribute('style','height:' + (23.6 * numbers[i]).toFixed(1) + 'px');
+
+		var newCap = document.createElement('div');
+		newCap.setAttribute('class','cap');
+		var newSpan = document.createElement('span');
+		var text = document.createTextNode(numbers[i]);
+		newSpan.appendChild(text);
+		newLoadedBrick.appendChild(newCap);
+		newLoadedBrick.appendChild(newSpan);
+		brickHolder.appendChild(newLoadedBrick);
+	}
+}
+// Tạo hàng xếp vào
+function createToLoadBricks(numbers) {
+	var cont = document.getElementsByClassName('cont')[0];
+	for (let i = 0, length = numbers.length; i < length; i++) {
+		var newToLoadedBrick = document.createElement('div');
+		newToLoadedBrick.setAttribute('class','to_load_brick under_start');
+		newToLoadedBrick.classList.add('element' + i);
+		newToLoadedBrick.setAttribute('style','height:' +  (20 + 24 * (numbers[i] - 1)).toFixed(1) + 'px; left: ' + (147 + 65 * i).toFixed(1) + 'px; top: ' + (323 - (20 + 24 * (numbers[i] - 1))) + 'px; touch-action: none;');
+		newToLoadedBrick.setAttribute('draggable','true');
+		var newCap = document.createElement('div');
+		newCap.setAttribute('class','cap');
+		var newSpan = document.createElement('span');
+		var text = document.createTextNode(numbers[i]);
+		newSpan.appendChild(text);
+		newToLoadedBrick.appendChild(newCap);
+		newToLoadedBrick.appendChild(newSpan);
+		cont.appendChild(newToLoadedBrick);
+	}
+}	
+
+/* Tiện ích */	
+// Chọn ngôn ngữ
+function selectLanguage() {
+	var languageMenu = document.getElementsByClassName('uchiru-locale-dropdown__menu_pull-right')[0];
+	if(languageMenu.style.display === 'none') {
+		languageMenu.style.display = 'block';
+	}
+	else languageMenu.style.display	 = 'none';
+}
 // Phát nút âm thanh hướng dẫn
 function play(){
 	$('audio').attr('src', 'server/Image/Load the trucks/3.mp3').get(0).play()
 }
 
+function searchNumber(str) {
+	var index = str.toString().search('element');
+	if(index === -1) return -1;
+	return str[index+7];
+}
